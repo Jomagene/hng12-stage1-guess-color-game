5221991c82b94533f5fc4b2b58acb75e93e9d498
Semjo <semjomagene@gmail.com>
2025-02-09 18:19:47 +0200

    Countdown implementation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,21 @@
+import { useState } from 'react';
+import Countdown from 'react-countdown';
+
 const Header = () => {
+  const [countdownDate] = useState(Date.now() + 30000);
+
+  const renderer = ({ minutes, seconds, completed }) => {
+    if (completed) {
+      return <span>End</span>;
+    } else {
+      return (
+        <span>
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </span>
+      );
+    }
+  };
+
   return (
     <header>
       <img src="/gameTitle.png" alt="" className="logo" />
@@ -6,8 +23,11 @@ const Header = () => {
         <img src="/reset-btn.png" alt="" />
         <img src="/quit-btn.png" alt="" />
       </nav>
-      <p>Time: 0</p>
+      <p>
+        Time: <Countdown date={countdownDate} renderer={renderer} />
+      </p>
     </header>
   );
 };
+
 export default Header;
