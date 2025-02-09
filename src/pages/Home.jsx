efe6e3a8a1918b8b9a67c278021007fd422431cf
Semjo <semjomagene@gmail.com>
2025-02-09 16:35:04 +0200

    Implement home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="main home">
+      <img src="/gameTitle.png" alt="game title" className="game-title" />
+      <img src="/start-btn.png" alt="start button" className="btn start" />
+    </div>
+  );
+};
+export default Home;
